c5a8399cf785b97e71aa1e463d7866177099c54c
n0t0rious <100134795+n0t0rious@users.noreply.github.com>
2023-12-20 18:13:48 +0200

    gitignore

---
+++ day-5/solution.py
@@ -0,0 +1,19 @@
+import re
+
+
+def main():
+    with open("input.txt") as f:
+        lines = f.read().splitlines()
+    print(f'Part 1: {part1(lines)}, Part 2: {part2(lines)}')
+
+
+def part1(lines):
+    ...
+
+
+def part2(lines):
+    ...
+
+
+if __name__ == "__main__":
+    main()
